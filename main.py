d286f2d10090e7de0fdc9ee176b7fa5291adb278
Saarthak <saarthakgupta19@gmail.com>
2024-12-23 14:57:05 +0800

    reformat

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-'''
+"""
 i. process speech request via OpenAI Wrapper
 ii. Translate into an array
 iii. Call the actual RBP dispensing instruction (eg. stop —> dispense —> delay)
-'''
+"""
+
 
 def processSpeechRequest():
     pass
 
+
 def convertToArray():
     pass
 
+
 def dispenseRBP():
     pass
-

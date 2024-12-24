1605bfa76db9e11c5f691fdd35bf379f007acad5
varun <vdonde@andrew.cmu.edu>
2024-12-24 20:58:52 +0700

    refactored main, dispense, added RBP integration, fixed main

--- dispense.py
+++ dispense.py
@@ -1,25 +1,37 @@
-# create the file for dispensing instruction
+"""
+Module for dispensing drinks
 
-# This is a draft for the main functionality of the dispenser
-import random
-
-
-# Assuming we generate random volumes to dispense
-def generate_dispensing_volumes(num_pumps=6, total_volume=100):
-    # Generate (num_pumps - 1) random cut points between 1 and total_volume-1
-    cut_points = sorted(random.sample(range(1, total_volume), num_pumps - 1))
-
-    # Add start and end to the cut points for slicing
-    cut_points = [0] + cut_points + [total_volume]
-
-    # Generate the volumes as differences between consecutive cut points
-    volumes_to_dispense = [cut_points[i + 1] - cut_points[i] for i in range(num_pumps)]
-
-    return volumes_to_dispense
+Contains dispense function, which takes in an array of ratios and
+interfaces with Raspberry Pi pins to dispense drink
+"""
 
 
-dispensing_volumes = generate_dispensing_volumes()
+# TODO: write ratio_to_time once hardware is assembled
+# TODO: check if any other functions for dispensing are needed
 
-# Output results
-print("Dispensing Volumes:", dispensing_volumes)
-print("Sum of Volumes:", sum(dispensing_volumes))
+#--------------------IMPORTS--------------------
+from machine import Pin
+import time
+
+#--------------------CONSTANTS--------------------
+PUMPS = [Pin(i, Pin.OUT) for i in range(6)]
+
+#--------------------HELPERS--------------------
+# @brief Converts a given ratio (1-100) to a dispensing time in seconds
+
+def ratio_to_time(ratio): 
+    pass
+
+#--------------------FUNCTIONS--------------------
+def dispense(ratios):
+    for i, pump in enumerate(PUMPS):
+        # Get ratio
+        ratio = ratios[i]
+        # Convert to duration
+        duration = ratio_to_time(ratio)
+        if duration > 0: 
+            pump.on()
+            time.sleep(duration)
+            pump.off()
+
+    return
--- openAI_methods.py
+++ openAI_methods.py
@@ -1,10 +1,19 @@
-# open ai api key methods
+"""
+Module for interfacing with OpenAI API
+
+Contains functions for generating volumes of each drink via natural language
+processing (NLP) using the OpenAI API.
+
+"""
 
 import openai
 
 # Set up OpenAI API key
 openai.api_key = "insert_api_key"
 
+
+# @brief Generates volumes of each drink via NLP
+# Example Response: [20, 20, 20, 20, 20, 20]
 
 # Generates volumes of each drink via NLP
 def generate_volumes(prompt):
@@ -14,4 +23,4 @@ def generate_volumes(prompt):
         max_tokens=100
     )
     print(response)  # To see what all is generated
-    return response.choices[0].text.strip()
+    return response.choices[0].text.strip() # TODO: does this actually return a list
